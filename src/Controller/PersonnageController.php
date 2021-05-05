0571bcb1f1db927e707b602bc381c7c060fe7f57
kenjy <kenjyzakary@hotmail.com>
2021-05-05 10:23:08 +0200

    first commit

---
+++ src/Controller/PersonnageController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PersonnageController extends AbstractController
+{
+    /**
+     * @Route("/personnage", name="personnage")
+     */
+    public function index(): Response
+    {
+        return $this->render('personnage/index.html.twig', [
+            'controller_name' => 'PersonnageController',
+        ]);
+    }
+}
