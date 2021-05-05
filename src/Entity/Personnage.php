1711941b9d4d085b56beda05fa19a7188d2b3497
kenjy <kenjyzakary@hotmail.com>
2021-05-05 23:26:25 +0200

    fourth commit

--- src/Entity/Personnage.php
+++ src/Entity/Personnage.php
@@ -4,10 +4,10 @@ namespace App\Entity;
 
 class Personnage {
 
-     public $nom;
-     public $age;
-     public $sexe;
-     public $carac = [];
+     private $nom;
+     private $age;
+     private $sexe;
+     private $carac = [];
 
      public static $personnages=[];
 
@@ -21,20 +21,51 @@ class Personnage {
 
      }
 
+     public function getNom(){
+          return $this->nom;
+     }
+     public function getAge(){
+          return $this->age;
+     }
+     public function getSexe(){
+          return $this->sexe;
+     }
+     public function getCarac(){
+          return $this->carac;
+     }
+
+     public function SetNom($nom){
+          $this->nom = $nom;
+          return $this;
+     }
+     public function setAge($age){
+          $this->age = $age;
+          return $this;
+     }
+     public function setSexe($sexe){
+          $this->sexe = $sexe;
+          return $this;
+     }
+     public function setCarac($carac){
+          $this->carac = $carac;
+          return $this;
+     }
+ 
+
      public static function creerPersonnages() {
-          $p1 = new Personnage("Marc", 25, true, [
+          new Personnage("Marc", 25, true, [
                "force" => 30,
                "agi" => 2,
                "intel" => 33,         
           ]);
 
-          $p2 = new Personnage("Milo", 30, true, [
+          new Personnage("Milo", 30, true, [
                "force" => 8,
                "agi" => 86,
                "intel" => 3,         
           ]);
 
-          $p3 = new Personnage("Tya", 20, false, [
+          new Personnage("Tya", 20, false, [
                "force" => 22,
                "agi" => 12,
                "intel" => 51,         
